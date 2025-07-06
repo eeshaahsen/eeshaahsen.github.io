73f32fe74aece128cf7a32fdee63160d6bdba591
Eesha Ahsen <eeshaahsen@gmail.com>
2025-07-06 18:15:03 +0200

    confetti added

--- script.js
+++ script.js
@@ -43,11 +43,13 @@ panels.forEach(panel => {
   leftBtn.addEventListener('click', () => {
     indices[category] = (indices[category] - 1 + imageFiles[category].length) % imageFiles[category].length;
     centerImg.src = `${category}/${imageFiles[category][indices[category]]}`;
+    checkForMatch();
   });
 
   rightBtn.addEventListener('click', () => {
     indices[category] = (indices[category] + 1) % imageFiles[category].length;
     centerImg.src = `${category}/${imageFiles[category][indices[category]]}`;
+    checkForMatch();
   });
 });
 
@@ -65,5 +67,53 @@ shuffleBtn.addEventListener('click', () => {
 
     // Update the image src
     centerImg.src = `${category}/${imageFiles[category][indices[category]]}`;
+    checkForMatch();
   });
 });
+
+function shuffleAllPanels() {
+  panels.forEach(panel => {
+    const category = panel.dataset.category;
+    const centerImg = panel.querySelector('.panel-image');
+    const max = imageFiles[category].length;
+
+    // Pick a random index
+    indices[category] = Math.floor(Math.random() * max);
+
+    // Update image
+    centerImg.src = `${category}/${imageFiles[category][indices[category]]}`;
+    checkForMatch();
+  });
+}
+window.addEventListener('DOMContentLoaded', () => {
+  shuffleAllPanels();
+});
+shuffleBtn.addEventListener('click', shuffleAllPanels);
+
+function triggerConfetti() {
+  confetti({
+    particleCount: 120,
+    spread: 100,
+    origin: { y: 0.6 }
+  });
+}
+
+function showMatchPopup(name) {
+  // alert(`This is ${name}!`);
+}
+
+function checkForMatch() {
+  const currentNames = Object.keys(indices).map(category => {
+    return imageFiles[category][indices[category]];
+  });
+
+  const allMatch = currentNames.every(name => name === currentNames[0]);
+
+  if (allMatch) {
+    const matchedName = currentNames[0].replace('.png', '');
+    showMatchPopup(matchedName);
+    triggerConfetti();
+  }
+}
+
+
